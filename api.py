be08b7d6d5307d322a8dabd3253f50b7fe3fcbad
onze <slapexs@gmail.com>
2023-05-23 22:34:44 +0700

    :sparkles: Create api for check lotto

--- api.py
+++ api.py
@@ -1,13 +1,26 @@
 from fastapi import FastAPI
+from main import Checklotto
 
+checklotto_obj = Checklotto()
+
+def CheckNumberLength(input_number:str):
+    if len(input_number) < 6:
+        return f"Please enter all 6 numbers {6 - len(input_number)} more missing"
+    elif len(input_number) > 6:
+        return f"Sorry your number's more than 6 unit"
+    else:
+        return True
+        
 app = FastAPI()
-
 
 @app.get('/')
 async def root():
     return {'message': "Hi every body if you want to read document please go to /redoc"}
 
-
-@app.get('/latest')
-async def Checklatest():
-    return 'hi'
+@app.get('/latest/{input_number}')
+async def Checklatest(input_number:str):
+    isNumberTrue = CheckNumberLength(input_number)
+    if isNumberTrue == True:
+        return checklotto_obj.CheckIsWonlotto(input_number)
+    else:
+        return {'status': 'Number length invalid', 'message': isNumberTrue}
