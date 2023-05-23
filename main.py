eae058db300a709406a68646b47ed809366f1576
onze <slapexs@gmail.com>
2023-05-23 22:34:27 +0700

    :sparkles: Create feature check lotto

--- main.py
+++ main.py
@@ -1,32 +1,18 @@
 import requests
+class Checklotto:
+    def CheckIsWonlotto(self, number:str):
+        self.url_api = "https://lotto.api.rayriffy.com/latest"
 
-url_api = "https://lotto.api.rayriffy.com/latest"
+        self.res = requests.get(self.url_api).json()['response']
+        self.date = self.res['date']
+        self.prizes = self.res['prizes']
+        self.isWon = False
+        for i in range(len(self.prizes)):
+            if number in self.prizes[i]['number']:
+                self.isWon = i
 
-res = requests.get(url_api).json()['response']
-date = res['date']
-prizes = res['prizes']
-
-input_number = ""
-while len(input_number) != 6:
-    input_number = input("Enter number: ")
-    if len(input_number) < 6:
-        print(f"Please enter all 6 numbers {6 - len(input_number)} more missing")
-    elif len(input_number) > 6:
-        print(f"Sorry your number's more then 6 unit")
-    else:
-        pass
-
-
-def Checklotto(number:str):
-    isWon = False
-    for i in range(len(prizes)):
-        if number in prizes[i]['number']:
-            isWon = i
-
-    if isWon != False:
-        return f"You won!! {prizes[isWon]['name']} - Reward:{prizes[isWon]['reward']}"
-    else:
-        return f"Sorry is not your time, GG Go next"
-        
-print(Checklotto(input_number))
-
+        if self.isWon != False:
+            return {'status': 'Won','message': f"ยินดีด้วย!! คุณถูก{self.prizes[self.isWon]['name']} มูลค่า {self.prizes[self.isWon]['reward']} บาท"}
+        else:
+            
+            return {'status': 'Lose','message': "Sorry is not your time, GG Go next"}
