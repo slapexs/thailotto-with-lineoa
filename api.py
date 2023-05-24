fa174aac50d2abe76a9ddbeba6867c15fdcd36f8
onze <slapexs@gmail.com>
2023-05-24 16:23:36 +0700

    :sparkles: create webhook for line bot

--- api.py
+++ api.py
@@ -1,5 +1,22 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, status, Request
 from main import Checklotto
+from pydantic import BaseModel
+import requests
+
+from linebot import LineBotApi, WebhookHandler
+from linebot.exceptions import InvalidSignatureError
+from linebot.models import MessageEvent, TextMessage, TextSendMessage
+from dotenv import dotenv_values
+
+config = dotenv_values('.env')
+lineBotApi = LineBotApi(config['ACCESS_TOKEN'])
+handler = WebhookHandler(config['SECRET'])
+
+app = FastAPI()
+
+class TestModel(BaseModel):
+    name:str
+    signature:str
 
 checklotto_obj = Checklotto()
 
@@ -11,8 +28,6 @@ def CheckNumberLength(input_number:str):
     else:
         return True
         
-app = FastAPI()
-
 @app.get('/')
 async def root():
     return {'message': "Hi every body if you want to read document please go to /redoc"}
@@ -24,3 +39,21 @@ async def Checklatest(input_number:str):
         return checklotto_obj.CheckIsWonlotto(input_number)
     else:
         return {'status': 'Number length invalid', 'message': isNumberTrue}
+
+@app.post("/webhook")
+async def webhook_handler(request: Request):
+    body = await request.body()
+    signature = request.headers["X-Line-Signature"]
+    try:
+        handler.handle(body.decode(), signature)
+    except InvalidSignatureError:
+        return "Invalid signature", 400
+    return "OK"
+
+@handler.add(MessageEvent, message=TextMessage)
+def handle_message(event: MessageEvent):
+    lineBotApi.reply_message(
+        event.reply_token,
+        TextSendMessage(text="You said: " + event.message.text)
+    )
+
